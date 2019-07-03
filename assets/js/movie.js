4c470bd0bde20757d18061707bfa94d9d8b335b4
Ubuntu <ubuntu@ip-172-31-92-111.ec2.internal>
2019-07-03 13:05:55 +0000

    added new graphs

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -7,26 +7,25 @@ function makeGraphs(error, lotrData) {
 
 
     run_time(ndx);
-
+    budgets(ndx);
+    revenue(ndx)
     dc.renderAll();
 
 }
 
+//---------------------------------------------------- Runtime Information---------------------------------
+
 function run_time(ndx) {
 
-    // var dim = ndx.dimension(dc.pluck('Name'));
-    
-    
-    //var group = dim.group().reduceSum(dc.pluck('RuntimeInMinutes'));
 
 
     var minus_series = ndx.dimension(function(d) {
         if (d.Name === 'The Lord of the Rings Series') {
             return null;
         }
-        if (d.Name === 'The Hobbit Series'){ 
+        if (d.Name === 'The Hobbit Series') {
             return null;
-            
+
         }
         else {
             return d.Name;
@@ -44,7 +43,84 @@ function run_time(ndx) {
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .xAxisLabel('Gender')
+        .xAxisLabel('Movies')
         .yAxis().ticks(10);
 
 }
+
+//---------------------------------------------------- Runtime Information end---------------------------------
+
+
+//---------------------------------------------------- budget Information---------------------------------
+
+function budgets(ndx) {
+
+    var minus_series = ndx.dimension(function(d) {
+        if (d.Name === 'The Lord of the Rings Series') {
+            return null;
+        }
+        if (d.Name === 'The Hobbit Series') {
+            return null;
+
+        }
+        else {
+            return d.Name;
+        }
+    });
+
+    var group = minus_series.group().reduceSum(dc.pluck('BudgetInMillions'));
+
+    dc.barChart('#budgets')
+        .width(400)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(minus_series)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel('Movies')
+        .yAxis().ticks(10);
+
+}
+//---------------------------------------------------- budget Information end---------------------------------
+
+//---------------------------------------------------- revenue Information ---------------------------------
+
+
+
+function revenue(ndx) {
+
+    var minus_series = ndx.dimension(function(d) {
+        if (d.Name === 'The Lord of the Rings Series') {
+            return null;
+        }
+        if (d.Name === 'The Hobbit Series') {
+            return null;
+
+        }
+        else {
+            return d.Name;
+        }
+    });
+
+    var group = minus_series.group().reduceSum(dc.pluck('BoxOfficeRevenueInMillions'));
+
+    dc.barChart('#revenue')
+        .width(400)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(minus_series)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel('Movies')
+        .yAxis().ticks(10);
+
+
+console.log(minus_series);
+}
+
+
+
